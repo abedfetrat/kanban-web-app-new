6521d9f2b0945bcb7c7148965836297916e950f7
Abed Fetrat <abedfetrat@gmail.com>
2024-01-16 18:05:17 +0100

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,47 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
+  darkMode: "class",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        desktop: "1400px",
+      },
+      colors: {
+        primary: "#635FC7",
+        "primary-hover": "#A8A4FF",
+        danger: "#EA5555",
+        "danger-hover": "#FF9898",
+        "light-border": "#E4EBFA",
+        "dark-border": "#3E3F4E",
+        black: "#000112",
+        white: "#FFFFFF",
+        "very-dark-grey": "#20212C",
+        "dark-grey": "#2B2C37",
+        "medium-grey": "#828FA3",
+        "light-grey": "#F4F7FD",
+        cyan: "#49C4E5",
+        green: "#67E2AE",
       },
     },
+    boxShadow: {
+      "surface-dark": "0px 4px 6px 0px rgba(54, 78, 126, 0.10)",
+      "surface-light": "0px 4px 6px 0px rgba(54, 78, 126, 0.10)",
+    },
   },
-  plugins: [],
-}
-export default config
+  plugins: [
+    require("@headlessui/tailwindcss"),
+    require('@tailwindcss/forms'),
+    plugin(function ({ addVariant }) {
+      addVariant("hocus", ["&:hover", "&:focus"]);
+    }),
+  ],
+};
+
+export default config;
